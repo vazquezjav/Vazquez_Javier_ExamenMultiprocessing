f0fc720c1df2051c955ebf2732e09e8da2b829ac
vazquezjav <34308694+vazquezjav@users.noreply.github.com>
2020-06-04 12:06:32 -0500

    Commit final

--- Vazquez_Javier_ExamenMPI..py
+++ Vazquez_Javier_ExamenMPI..py
@@ -4,4 +4,58 @@ Created on Thu Jun  4 09:45:24 2020
 
 @author: javier
 """
+import time
+from mpi4py import MPI
+from random import randint
+import numpy as np
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+np.random.RandomState(100)
+
+
+filas=4000000
+#filas=20
+minimum=4
+maximum=8
+arr = np.random.randint(0, 10, size=[filas, 10])
+
+#ar = arr.tolist()
+start = time.time()
+if rank == 0:  
+    
+    averow = int(filas/(size-1))
+    extra = filas%(size-1)
+    offset = 0
+    
+    for dest in range(1,size):
+        rows = averow+1 if dest <= extra else averow 
+        comm.send(offset, dest)
+        comm.send(rows, dest)
+        comm.send(arr[offset:(rows+offset)],dest)
+        offset+=rows
+    
+    resultado=[]
+    for i in range(1,size):
+        extracto=comm.recv(source=i)
+        
+        resultado.append(extracto)
+    
+    
+if rank > 0:
+    offse=comm.recv(source=0)
+    row=comm.recv(source=0)
+    data=comm.recv(source=0)
+    #print(data)
+    res=[]
+    for i in data:
+        count = 0
+        for j in i:
+            if minimum <= j <= maximum:
+                count += 1
+        res.append(count)
+    comm.send(res,0)
+    
+end=time.time()-start
+print("Tiempo ",end)
--- Vazquez_Javier_ExamenMultiprocessing.py
+++ Vazquez_Javier_ExamenMultiprocessing.py
@@ -4,4 +4,85 @@ Created on Thu Jun  4 09:44:35 2020
 
 @author: javier
 """
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 09:53:11 2020
 
+@author: javier
+"""
+import time
+
+import numpy as np
+
+
+import multiprocessing
+import random
+import time
+
+class Examen(multiprocessing.Process):
+    def __init__(self, queue,minimum,maximum,arr,inicio,fin):
+        multiprocessing.Process.__init__(self)
+        self.queue = queue
+        self.minimum=minimum
+        self.maximum=maximum
+        self.arr=arr
+        self.inicio=inicio
+        self.fin=fin
+        self._return=None
+
+    def run(self) :
+        
+        for i in range(self.inicio,self.fin):
+            #print(self.arr[i])
+            self.queue.put(self.how_many_within_range_sequential(self.arr[i],self.minimum,self.maximum))
+            
+        self._return=self.queue
+        return self._return
+    
+        
+    def how_many_within_range_sequential(self,row, minimum, maximum):   
+        
+        count = 0
+        for n in row:
+            if minimum <= n <= maximum:
+
+                count += 1
+        
+        return count
+
+if __name__ == '__main__':
+
+
+    np.random.RandomState(100)
+
+    arr = np.random.randint(0, 10, size=[4000000, 10])
+
+    ar = arr.tolist()
+
+    start = time.time()
+
+    resultsSec = []
+    minimum=4
+    maximum=8
+    
+    res=[]
+    queue = multiprocessing.Queue()
+    queue2 = multiprocessing.Queue()
+    examen = Examen(queue,minimum,maximum,arr,0,round(len(arr)/2))
+    examen.start()
+    examen.join()
+    for i in range(queue.qsize()):
+        res.append(queue.get())
+    examen2 = Examen(queue2,minimum,maximum,arr,round(len(arr)/2),round(len(arr)))
+    
+    examen2.start()
+    examen2.join()
+    
+
+    for i in range(queue2.qsize()):
+        res.append(queue2.get())
+    print(len(res))
+    end =  time.time()-start
+    print("Tiempo: ",end)
+
+   
