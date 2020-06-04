e795506cac8cdf0a734622a8447ac9b68340b572
Javier Vazquez <vazquezjavier079@gmail.com>
2020-06-04 09:50:24 -0500

    primer commit

---
+++ Vazquez_Javier_ExamenMPI..py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 09:45:24 2020
+
+@author: javier
+"""
+
---
+++ Vazquez_Javier_ExamenMultiprocessing.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 09:44:35 2020
+
+@author: javier
+"""
+
